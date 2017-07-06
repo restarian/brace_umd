41b2c2cf226e187f1b3a1cf23f252087c0a9ddf1
restarian <robertsteckroth@gmail.com>
2017-07-06 14:16:44 -0500

    Initial commit of brace umd

---
+++ test/constructor.js
@@ -0,0 +1,47 @@
+#!mocha
+/*
+Copyright (c) 2017 Robert Edward Steckroth
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+  Brace UMD is a unified module definition script to use when defining javascript modules.
+
+  this file is a part of Brace UMD
+
+ Author: Robert Edward Steckroth II, Bustout, <RobertSteckroth@gmail.com>
+*/
+
+if (typeof define !== 'function') { var define = require('amdefine')(module) }
+
+define(["chai", "brace_umd"], function(chai, brace_umd) {
+
+ var expect = chai.expect
+
+	describe("Construction", function() {
+
+		//beforeEach(function() { })
+
+		it("", function() {
+		})
+
+		it("", function() {
+
+		})
+	})
+})
