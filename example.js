02cc0b188f8c0d4ae9a66a78bc39a389cd54f174
restarian <robertsteckroth@gmail.com>
2017-07-06 20:33:49 -0500

    added uglify3

--- example.js
+++ example.js
@@ -1,10 +1,11 @@
 
 
-var umd = require("brace_umd")
+// This is the unified module definition function from the build directory. It is just thee same as using the lib source file.
+var umd=function(e,n,o,t){3===arguments.length&&(t=o,o=n,n=void 0,arguments[4]=t,arguments.length=arguments.length+1),"function"==typeof define&&define.amd?(console.log("Using amdefine to define the module",n||""),arguments.length>=4?define(n,o,t):define(o,t)):"object"==typeof module&&module.exports?(console.log("Using CommonJS to define module",n||""),module.exports=t.apply(t,o.map(function(e,n){return require(e)}))):(console.log("Using the native global Object to define the module",n||""),n.toString()?o.every(function(o,t){return o in e||!!console.warn("The dependency",o,"is not loaded into the engine yet. Skipping loading of the module",n||"")})&&(e[n]=t.apply(t,o.map(function(n,o){return e(n)}))):console.warn("The global native Object is going to be used but the module is parameter is not available."))};umd(this,"umd",[],function(){return umd});
 
-// This is the unified module definition function. If amdefine is already available here than it will use define(). If not, than module.exports
-// or the global native Object will be used.
+//delete require.cache[module.children[0].id]
+if (typeof define !== 'function') { var define = require('amdefine')(module) }
+
 umd(this, "example_module", [], function() {
-
     console.log("Hello from example module")
 })
