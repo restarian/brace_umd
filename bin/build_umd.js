7fbdce5f6531ac4491a3e9f1111188750d6cf01b
restarian <robertsteckroth@gmail.com>
2017-07-09 22:52:39 -0400

    Add requirejs wrapping fragment building

--- bin/build_umd.js
+++ bin/build_umd.js
@@ -13,12 +13,18 @@ fs.readFile(lib+"/umd.js", (err, data) => {
   if (err) throw err
 
 	var compressed = uglify.minify(data.toString(), options) // parse code and get the initial AST
-
 	fs.writeFile(out, compressed.code, (err) => {
-  		if (err) throw err
+  	if (err) throw err
 		else console.log("Finished uglify build:", out)
 	})
 
+	fs.writeFile(lib + "/../builds/" + "wrap_start_umd"+config.version+".frag", compressed.code.substr(8, compressed.code.length-11), (err) => {
+  	if (err) throw err
+  	fs.writeFile(lib + "/../builds/" + "wrap_end_umd"+config.version+".frag", "}", (err) => {
+    	if (err) throw err
+  		else console.log("Finished uglify build wrapping.")
+  	})
+	})
 })
 
 
