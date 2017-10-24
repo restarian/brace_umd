5121dfb6537e7f4d59d043b1c9248263eac840bb
Robert Steckroth <robertsteckroth@gmail.com>
2017-10-24 08:59:11 -0400

    Another unit test design attempt

--- example/nodejs/amdefine/rjs_config_auto_anonymous.js
+++ example/nodejs/amdefine/rjs_config_auto_anonymous.js
@@ -2,14 +2,14 @@
 	"baseUrl": "./",
   	"dir": "build",
 	"optimize": "uglify2",
-	"uglify2": nodeRequire("brace_umd").build_option,
+	"uglify2": nodeRequire("../../../../brace_umd").build_option,
 	"modules": [
 		{ "name": "entry" },
 		{ "name": "base_module" }
 	],
 	"wrap": {
 		// r.js redefines require and define again so make sure to use nodeRequire which will have the original require statement.
-		"start": nodeRequire("brace_umd").wrap_start,
-		"end": nodeRequire("brace_umd").wrap_end_option({"auto_anonymous": true})
+		"start": nodeRequire("../../../../brace_umd").wrap_start,
+		"end": nodeRequire("../../../../brace_umd").wrap_end_option({"auto_anonymous": true})
 	}
 }
